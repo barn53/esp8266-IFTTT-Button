0019716dbd1730c424a0889712cd3008d1c3c6d4
gerdmuller <gerdmuller.de@gmail.com>
2019-01-03 15:08:40 +0100

    now functioning version! - yeah!

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,47 @@
 #include <Arduino.h>
 #include <Ticker.h>
 
+#include <ESP8266HTTPClient.h>
 #include <ESP8266WiFi.h>
-#include <WiFiClient.h>
 
 #include "secrets.h"
 
 Ticker t1;
-Ticker t2;
-WiFiClient client;
 
 void t1sr()
 {
     digitalWrite(D1, !digitalRead(D1));
+    digitalWrite(D2, !digitalRead(D1));
 }
-void t2sr()
+
+ADC_MODE(ADC_VCC);
+
+HTTPClient http;
+
+void startRoutine()
 {
-    digitalWrite(D2, !digitalRead(D2));
-    digitalWrite(D8, !digitalRead(D8));
+    t1.attach(0.05, t1sr);
+}
+
+void offRoutine(bool ok)
+{
+    t1.detach();
+    if (ok) {
+        digitalWrite(D1, HIGH);
+        digitalWrite(D2, LOW);
+    } else {
+        digitalWrite(D1, LOW);
+        digitalWrite(D2, HIGH);
+    }
+
+    delay(2000);
+
+    digitalWrite(D7, HIGH); // off
+    digitalWrite(D8, HIGH); // off
+
+    // not reached, when hardware switches off, but otherwise...
+    Serial.println("ESP.restart()");
+    ESP.restart();
 }
 
 void setupWiFi()
@@ -26,62 +50,121 @@ void setupWiFi()
     WiFi.hostname(hostname);
 
     Serial.print("Connecting ");
+    int counter(0);
+    bool ok(true);
     while (WiFi.status() != WL_CONNECTED) {
-        delay(500);
+        delay(100);
+        if (counter > 50) {
+            ok = false;
+            break;
+        }
+        ++counter;
         Serial.print(".");
     }
 
     String s;
+    if (ok) {
+        s = "Connected\n IP address: ";
+        s += WiFi.localIP().toString();
+        s += "\n Hostname: ";
+        s += WiFi.hostname();
+    } else {
+        s = "Connection failed!";
+        Serial.println(s);
 
-    s = "Connected\n IP address: ";
-    s += WiFi.localIP().toString();
-    s += "\n Hostname: ";
-    s += WiFi.hostname();
+        offRoutine(ok);
+    }
 
     Serial.println(s);
 }
 
-void connect()
+bool send(const String& json, String& response)
 {
-    int port(1130);
-    Serial.printf("\n[Connecting to %s ... ", host);
-    if (client.connect(host, port)) {
-        Serial.println("connected]");
-        Serial.println("[Sending a request]");
-        client.print(String(R"(GET /?o={"command":"ticket"})") + " HTTP/1.1\r\n" + "Host: " + host + "\r\n" + "Connection: close\r\n" + "\r\n");
-        Serial.println("[Response:]");
-        while (client.connected() || client.available()) {
-            if (client.available()) {
-                String line = client.readStringUntil('\n');
-                Serial.println(line);
-            }
+    response = "";
+    bool ret(false);
+    String url;
+    url = "/?o=";
+    url += json;
+    Serial.println(url);
+
+    http.begin(host, port, url);
+    int httpCode = http.GET();
+    if (httpCode) {
+        if (httpCode == 200) {
+            response = http.getString();
+            ret = true;
         }
-        client.stop();
-        Serial.println("\n[Disconnected]");
-    } else {
-        Serial.println("connection failed!]");
-        client.stop();
     }
+    http.end();
+    return ret;
+}
+
+bool getTicket(String& ticket)
+{
+    String chipID(ESP.getChipId());
+    String vcc(ESP.getVcc() / 1024.f);
+    String json(R"({"command":"ticket"})");
+
+    return send(json, ticket);
+}
+
+bool command(const String& ticket, const String& cmd, String& response)
+{
+    String chipID(ESP.getChipId());
+    String vcc(ESP.getVcc() / 1024.f);
+
+    String json(R"({"command":")");
+    json += cmd;
+    json += R"(","ticket":")";
+    json += ticket;
+    json += R"(","user":")";
+    json += user;
+    json += R"(","vcc":)";
+    json += vcc;
+    json += R"(,"chip-id":")";
+    json += chipID;
+    json += R"(","latitude":)";
+    json += latitude;
+    json += R"(,"longitude":)";
+    json += longitude;
+    json += "}";
+
+    return send(json, response);
 }
 
 void setup()
 {
+    pinMode(D8, OUTPUT); // GPIO15, off
+    pinMode(D7, OUTPUT); // GPIO13, off
+    pinMode(D1, OUTPUT); // GPIO5, ok
+    pinMode(D2, OUTPUT); // GPIO4, fail
+
+    digitalWrite(D8, LOW);
+    digitalWrite(D7, LOW);
+    digitalWrite(D1, LOW);
+    digitalWrite(D2, LOW);
+
     Serial.begin(115200);
+    Serial.println("Hallo...");
 
-    pinMode(D1, OUTPUT);
-    pinMode(D2, OUTPUT);
-    pinMode(D8, OUTPUT);
-
-    t1.attach(0.7, t1sr);
-    t2.attach(0.3, t2sr);
-
-    Serial.printf("Hallo...");
+    startRoutine();
 
     setupWiFi();
 }
 
 void loop()
 {
-    connect();
-    delay(5000);
+    String ticket;
+    String response;
+    bool ok(false);
+
+    if (getTicket(ticket)) {
+        Serial.println(ticket);
+        if (command(ticket, "door", response)) {
+            Serial.println(response);
+            ok = true;
+        }
+    }
+
+    offRoutine(ok);
 }
--- src/rename_to_secrets.h
+++ src/rename_to_secrets.h
@@ -4,3 +4,7 @@ static const char* ssid = "ssid";
 static const char* password = "password";
 static const char* hostname = "whatever";
 static const char* host = "myhost.com";
+static const char* user = "user";
+static int port = 80;
+static float latitude = 0.;
+static float longitude = 0.;
